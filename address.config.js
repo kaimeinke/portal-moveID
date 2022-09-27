fa9cdaa6f1e24bcc4e11408c5804936c4b70663f
Kai Meinke <kai.meinke@posteo.de>
2022-09-27 11:34:09 +0200

    Update address.config.js

--- address.config.js
+++ address.config.js
@@ -7,8 +7,10 @@ module.exports = {
     {
       title: 'Mobility',
       assets: [
-        'did:op:2c224e12205a100534E4aBAf9bAB7b983a9F1fBE',
-        'did:op:5c06Cff5a48aF3267c2bc913DeCEb2384c0f8a10'
+        'did:op:00dd3550532A6dA0d0427E8ECbE4f40F315C1F3B',
+        'did:op:4b42dfc40Ae829968De8142eb35f38a8bD46D5FA',
+        'did:op:4bB5353309913f8003a9451B8E0C15C81dCB9064',
+        'did:op:6865B70CddB1041f49d83c9c46D676f16D8Bd496'
       ]
     }
   ]
