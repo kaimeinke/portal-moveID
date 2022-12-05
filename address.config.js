9310e937f8ed92f2e5a0c3de6d3e38149a10f78a
Kai Meinke <kai.meinke@posteo.de>
2022-12-05 09:05:26 +0100

    Add Road Damage Case

--- address.config.js
+++ address.config.js
@@ -10,7 +10,9 @@ module.exports = {
         'did:op:a905d250B00b4CBd483225Bc3cF9E3cf8b02ac39',
         'did:op:BDCD96faF8c71E2ad3028eF267d6C6D6702F311f',
         'did:op:5695670bbFC17d7f9aA5afaA582D27AC798DD6A3',
-        'did:op:d51494c94d3aE025794034431f053e3fb6a26815'
+        'did:op:d51494c94d3aE025794034431f053e3fb6a26815',
+        'did:op:78f831361E1c850a16667346068f5fcAFE83F1F5',
+        'did:op:dAd9D741C9BC580a7b550dac42C9bf1aAe582B5c'
       ]
     }
   ]
