1ec1a711adc82b31cfc04cf6ba53b654bbc89548
Kai Meinke <kai.meinke@posteo.de>
2023-02-08 10:05:44 +0100

    Update Landing Page

--- address.config.js
+++ address.config.js
@@ -12,7 +12,11 @@ module.exports = {
         'did:op:5695670bbFC17d7f9aA5afaA582D27AC798DD6A3',
         'did:op:d51494c94d3aE025794034431f053e3fb6a26815',
         'did:op:dAd9D741C9BC580a7b550dac42C9bf1aAe582B5c',
-        'did:op:78f831361E1c850a16667346068f5fcAFE83F1F5'
+        'did:op:78f831361E1c850a16667346068f5fcAFE83F1F5',
+        'did:op:5E2cd5bCC8ab1A6227b54626ecc9481cE18ACBF9',
+        'did:op:76b92C812Ac384425865329b1162fea6Af9246D1',
+        'did:op:48c016C651B9b57E146364DAFaA17C7D5AE3C5Cf',
+        'did:op:8f496FbBe70255688298419308ac125d2e8080B6'
       ]
     }
   ]
