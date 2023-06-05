fe5ef446e5df3f00ff07875babb9b146983dd1ec
Luca Milanese <luca.milanese90@gmail.com>
2023-06-05 15:27:04 +0200

    feat: update "funded by" image

--- src/components/Home/FundedBy/FundedBy.tsx
+++ src/components/Home/FundedBy/FundedBy.tsx
@@ -7,10 +7,7 @@ export default function FundedBy(): ReactElement {
     <Container className={styles.wrapper}>
       <h3>Funded By</h3>
       <div className={styles.container}>
-        <img
-          className={styles.logo}
-          src="images/BMWE_Fz_2017_Office_Farbe_en.png"
-        />
+        <img className={styles.logo} src="images/BMWK-EU_funded_en_RGB.png" />
       </div>
     </Container>
   )
