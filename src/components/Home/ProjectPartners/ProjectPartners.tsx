2bda1ecd7fb84c6d3765056f769aa8b6c00482ad
Luca Milanese <luca.milanese90@gmail.com>
2023-11-03 16:50:09 +0100

    chore: remove React default import

--- src/components/Home/ProjectPartners/ProjectPartners.tsx
+++ src/components/Home/ProjectPartners/ProjectPartners.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement } from 'react'
+import { ReactElement } from 'react'
 import styles from './ProjectPartners.module.css'
 import Carousel from '@components/@shared/Carousel/Carousel'
 import Container from '@components/@shared/atoms/Container'
